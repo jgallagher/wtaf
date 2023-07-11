5e3b6f1980ab5b8700aa54469d4aecb639ab0251
John Gallagher <john@oxidecomputer.com>
2023-07-11 14:02:53 -0400

    add balloon

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,10 @@ use bytes::Bytes;
 use futures::StreamExt;
 use reqwest::Client;
 use sha2::{Digest, Sha256};
-use std::mem;
+use std::{env, mem, sync::Arc};
 use tokio::sync::mpsc;
 
-async fn get_it(client: Client, url: String) {
+async fn get_it(correct: Arc<[u8]>, client: Client, url: String) {
     let response = client.get(&url).send().await.expect("GET failed");
     let mut stream = response.bytes_stream();
     let mut n = 0;
@@ -15,8 +15,19 @@ async fn get_it(client: Client, url: String) {
     while let Some(item) = stream.next().await {
         let chunk = item.expect("error on item");
         if !chunk.is_empty() {
-            if chunk.iter().all(|&x| x == 0) {
-                println!("[get-it] all zero chunk: offset {n}");
+            if chunk != correct[n..][..chunk.len()] {
+                println!(
+                    "[get-it] incorrect data at offset {n:#x} (addr={:?})",
+                    chunk.as_ref().as_ptr()
+                );
+                for i in 0..chunk.len() {
+                    println!(
+                        "offset={:#10x} expected={:02x} got={:02x}",
+                        n + i,
+                        correct[n + i],
+                        chunk[i]
+                    );
+                }
             }
             n += chunk.len();
             tx.send(chunk).await.unwrap();
@@ -50,10 +61,27 @@ async fn hash_it(mut rx: mpsc::Receiver<Bytes>) {
 
 #[tokio::main]
 async fn main() {
-    let url = std::env::args().nth(1).expect("need url as first arg");
+    let mut args = env::args();
+    _ = args.next(); // skip argv[0]
+    let url = args.next().expect("need url as arg 1");
+    let path = args.next().expect("need path to correct file as arg 2");
+    let data: Arc<[u8]> = std::fs::read(&path)
+        .expect("failed to read correct file")
+        .into();
+
+    let balloon_size = args
+        .next()
+        .expect("need size of balloon as arg 3")
+        .parse::<usize>()
+        .expect("failed to parse balloon size arg");
+
+    let v = vec![1u8; balloon_size];
+    println!("big v = {}", v[balloon_size - 1]);
+
     let client = Client::new();
 
     loop {
-        get_it(client.clone(), url.clone()).await;
+        println!("big v = {}", v[balloon_size - 1]);
+        get_it(Arc::clone(&data), client.clone(), url.clone()).await;
     }
 }
